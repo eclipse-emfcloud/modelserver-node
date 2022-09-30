9ec1a784190c092c59447b92a76f2c57a9304814
Christian W. Damus <give.a.damus@gmail.com>
2022-09-30 09:59:52 -0400

    Express routing enhancements
    - router identification for targeted middleware contributions
    - contribution of middlewares after core route handling
    - backstop for custom route handlers to terminate next() chain
    
    Resolves #47
    
    Contributed on behalf of STMicroelectronics.
    
    Signed-off-by: Christian W. Damus <cdamus.ext@eclipsesource.com>

---
+++ packages/modelserver-node/src/routing.spec.ts
@@ -0,0 +1,195 @@
+/********************************************************************************
+ * Copyright (c) 2022 STMicroelectronics.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0, or the MIT License which is
+ * available at https://opensource.org/licenses/MIT.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
+ *******************************************************************************/
+import { MiddlewareProvider, RouteProvider } from '@eclipse-emfcloud/modelserver-plugin-ext';
+import axios, { AxiosInstance } from 'axios';
+import * as chai from 'chai';
+import { expect } from 'chai';
+import * as chaiLike from 'chai-like';
+import * as express from 'express';
+import { IRouter, RequestHandler } from 'express';
+import * as http from 'http';
+import * as sinon from 'sinon';
+import { assert } from 'sinon';
+
+import { createContainer } from './di';
+import { ModelServer } from './server';
+
+/**
+ * Tests for integration of _Express_ routers and middlewares contributed by `RouteProvider`s and `MiddlewareProvider`s.
+ */
+export const routingTests = undefined;
+
+chai.use(chaiLike);
+
+type MockMiddlewareProvider = sinon.SinonSpy<[router: IRouter, route?: string, routerId?: string], RequestHandler[]>;
+
+const testRoutes = '/api/v0/test';
+const route1 = testRoutes + '/route1';
+const route2 = testRoutes + '/route2';
+const route3 = testRoutes + '/route3';
+const testRouterID = '$test$router';
+const requestTestDataKey = '$test$data';
+
+describe('Routing Tests', () => {
+    let server: ModelServer;
+
+    const data: string[] = [];
+    let mockMWP: MockMiddlewareProvider;
+
+    let client: AxiosInstance;
+
+    before(() => {
+        client = axios.create({ baseURL: 'http://localhost:8082' });
+    });
+
+    beforeEach(async () => {
+        const container = await createContainer(8082, 'error');
+
+        container.bind(RouteProvider).toConstantValue(testRouteProvider());
+        mockMWP = sinon.spy((_router, _route, _routerId) => []);
+        container.bind(MiddlewareProvider).toConstantValue({ getMiddlewares: mockMWP });
+        container.bind(MiddlewareProvider).toConstantValue(testBeforeMiddlewares(data));
+        container.bind(MiddlewareProvider).toConstantValue(testAfterMiddlewares());
+
+        server = container.get(ModelServer);
+        await server.serve(8082, 8083);
+    });
+
+    afterEach(async () => {
+        await server.stop();
+        data.splice(0, data.length);
+    });
+
+    it('Router identification', () => {
+        assert.calledWith(
+            mockMWP,
+            // An `app` has a `_router` property but a `router` does not
+            sinon.match(actual => !('_router' in actual)),
+            testRoutes,
+            testRouterID
+        );
+    });
+
+    it('Middleware ordering', async () => {
+        await client.get(route1);
+        expect(data).to.include.ordered.members(['before', 'main handler', 'after']);
+    });
+
+    it('Middleware after all routers', async () => {
+        await client.get(route1);
+        expect(data).to.have.length.greaterThanOrEqual(1);
+        expect(data[data.length - 1]).to.be.eq('after all');
+    });
+
+    describe('Forwarding backstop', async () => {
+        let upstream: express.Application;
+        let upstreamServer: http.Server;
+        let route3Spy: sinon.SinonSpy;
+
+        before(async () => {
+            upstream = express();
+            upstream.use(express.json());
+            upstream.get(route2, (_req, res) => res.json({ success: true }));
+            route3Spy = sinon.spy((_req, res) => res.json({ success: 'upstream' }));
+            upstream.get(route3, route3Spy);
+            upstreamServer = upstream.listen(8083);
+        });
+
+        after(async () => {
+            upstreamServer.close();
+        });
+
+        it('Pass to upstream', async () => {
+            const res = await client.get(route2);
+            expect(res.status).to.be.eq(200);
+            expect(res.data).to.be.like({ success: true });
+        });
+
+        it('Blocked from upstream', async () => {
+            const res = await client.get(route3);
+
+            expect(res.status).to.be.eq(200);
+            expect(res.data).to.be.like({ success: 'local' });
+
+            assert.notCalled(route3Spy);
+        });
+    });
+});
+
+function testRouteProvider(): RouteProvider {
+    return {
+        configureRoutes: routerFactory => {
+            const router = routerFactory(testRoutes, { routerId: testRouterID });
+            router.get('/route1', (req, res, next) => {
+                if (requestTestDataKey in req) {
+                    const data = req[requestTestDataKey] as string[];
+                    data.push('main handler');
+                }
+                res.json({ success: true, message: 'Main handler called.' });
+                return next();
+            });
+            router.get('/route3', (_req, res, next) => {
+                res.json({ success: 'local' });
+                next();
+            });
+
+            const router2 = routerFactory(route2, { forwardToUpstream: true });
+            router2.get('/', (_req, _res, next) => next()); // Just delegate
+        }
+    };
+}
+
+function testBeforeMiddlewares(data: string[]): MiddlewareProvider {
+    return {
+        getMiddlewares: (router: IRouter, route?: string, routerId?: string) => {
+            if (route === testRoutes) {
+                return [
+                    (req, _res, next) => {
+                        data.push('before');
+                        req[requestTestDataKey] = data;
+                        return next();
+                    }
+                ];
+            }
+            return [];
+        }
+    };
+}
+
+function testAfterMiddlewares(): MiddlewareProvider {
+    return {
+        getAfterMiddlewares: (router: IRouter, route?: string, routerId?: string) => {
+            if (!route) {
+                // Install a middleware on the root Express `app` after all of the various routers
+                return [
+                    (req, _res, next) => {
+                        if (req.path === route1 && requestTestDataKey in req) {
+                            const data = req[requestTestDataKey] as string[];
+                            data.push('after all');
+                        }
+                        return next();
+                    }
+                ];
+            } else if (route === testRoutes) {
+                return [
+                    (req, _res, next) => {
+                        if (requestTestDataKey in req) {
+                            const data = req[requestTestDataKey] as string[];
+                            data.push('after');
+                        }
+                        return next();
+                    }
+                ];
+            }
+            return [];
+        }
+    };
+}
--- packages/modelserver-node/src/server.ts
+++ packages/modelserver-node/src/server.ts
@@ -8,10 +8,10 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR MIT
  *******************************************************************************/
-import { Logger, MiddlewareProvider, RouteProvider } from '@eclipse-emfcloud/modelserver-plugin-ext';
+import { Logger, MiddlewareProvider, RouteProvider, RoutingOptions } from '@eclipse-emfcloud/modelserver-plugin-ext';
 import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
 import * as express from 'express';
-import { RequestHandler } from 'express';
+import { Request, RequestHandler } from 'express';
 import * as asyncify from 'express-asyncify';
 import * as expressWS from 'express-ws';
 import { WebsocketRequestHandler } from 'express-ws';
@@ -22,6 +22,25 @@ import * as WebSocket from 'ws';
 import { InternalModelServerClientApi } from './client/model-server-client';
 import { handleClose, handleError, WSUpgradeRequest } from './client/web-socket-utils';
 import { InternalModelServerPluginContext } from './plugin-context';
+
+const STANDARD_ROUTES = new Set([
+    '/models',
+    '/modelelement',
+    '/modeluris',
+    '/server/ping',
+    '/server/configure',
+    '/subscribe',
+    '/close',
+    '/save',
+    '/saveall',
+    '/undo',
+    '/redo',
+    '/transaction',
+    '/validation',
+    '/validation/constraints',
+    '/typeschema',
+    '/uischema'
+]);
 
 /**
  * The _Model Server_ core.
@@ -50,6 +69,8 @@ export class ModelServer {
         this.pluginContext.initializePlugins();
     }
 
+    protected readonly backstopPaths = new Set<string>();
+
     protected server: http.Server;
 
     /**
@@ -64,29 +85,58 @@ export class ModelServer {
         const { applyTo: wsify, app } = asyncify(expressWS(express(), undefined, { leaveRouterUntouched: true }));
         app.use(express.json());
 
-        // Use provided middlewares that are applicable globally
-        this.middlewareProviders.flatMap(p => p.getMiddlewares(app)).forEach(mw => app.use(mw));
+        // Use provided before-middlewares that are applicable globally
+        this.middlewareProviders.flatMap(p => p.getMiddlewares?.(app) ?? []).forEach(mw => app.use(mw));
 
         // Isolate contributed route handlers each in their own router.
         const routes = {
-            routers: [],
-            factory: (route: string) => {
+            routers: [] as Array<{ route: string; router: express.Router; options?: RoutingOptions }>,
+            factory: (route: string, options?: RoutingOptions) => {
                 const newRouter = express.Router();
                 wsify(newRouter);
 
-                // Apply provided route-specific middlewares
-                this.middlewareProviders.flatMap(p => p.getMiddlewares(newRouter, route)).forEach(mw => newRouter.use(mw));
+                // Apply provided route-specific _before_ middlewares
+                this.middlewareProviders
+                    .flatMap(p => p.getMiddlewares?.(newRouter, route, options?.routerId) ?? [])
+                    .forEach(mw => newRouter.use(mw));
 
-                routes.routers.push({ route, router: newRouter });
+                routes.routers.push({ route, router: newRouter, options });
                 return newRouter;
             },
-            install: () => routes.routers.forEach(r => app.use(r.route, r.router))
+            install: () =>
+                routes.routers.forEach(r => {
+                    // Install middlewares after the route handlers
+                    this.middlewareProviders
+                        .flatMap(p => p.getAfterMiddlewares?.(r.router, r.route, r.options?.routerId) ?? [])
+                        .forEach(mw => r.router.use(mw));
+
+                    // Do we need a backstop to prevent `next()` delegation to the upstream server?
+                    if (!r.options?.forwardToUpstream) {
+                        // Record the routes supported by this router as needing to be backstopped.
+                        // By default, do not backstop (i.e., allow to pass through) routes that are
+                        // known to be supported by the upstream server because they are core
+                        // Model Server API, unless the options expressly deny with an actual falsy value
+                        const explicitlyBackstopped = typeof r.options?.forwardToUpstream !== 'undefined';
+
+                        r.router.stack
+                            .map(layer => r.route + (layer?.route?.path ?? ''))
+                            .map(handledRoute => handledRoute.replace(/\/+$/, ''))
+                            .filter(handledRoute => explicitlyBackstopped || !this.isModelServerRoute(handledRoute))
+                            .forEach(backstopped => this.backstopPaths.add(backstopped));
+                    }
+
+                    // And install the router in the app
+                    app.use(r.route, r.router);
+                })
         };
 
         for (const routing of this.routeProviders) {
             routing.configureRoutes(routes.factory);
         }
         routes.install();
+
+        // Use provided after-middlewares that are applicable globally
+        this.middlewareProviders.flatMap(p => p.getAfterMiddlewares?.(app) ?? []).forEach(mw => app.use(mw));
 
         const upstream = axios.create({ baseURL: `http://localhost:${upstreamPort}/` });
 
@@ -136,13 +186,18 @@ export class ModelServer {
                 return;
             }
 
+            if (this.shouldBackstop(req)) {
+                // Don't forward to upstream
+                return;
+            }
+
             const relayReq: AxiosRequestConfig = {
                 url: req.url,
                 method: req.method.toUpperCase() as Method,
                 data: req.body
             };
 
-            this.logger.debug(`Forwarding ${req.method} request to Upstream Model Server.`);
+            this.logger.debug(`Forwarding ${req.method} request on ${req.url} to Upstream Model Server.`);
 
             upstream
                 .request(relayReq)
@@ -204,6 +259,15 @@ export class ModelServer {
                 handleError('upstream', this.logger, downstream)(error);
             }
         };
+    }
+
+    protected shouldBackstop(req: Request): boolean {
+        return this.backstopPaths.has(req.path);
+    }
+
+    protected isModelServerRoute(route: string): boolean {
+        const match = route.match(/^\/api\/v[0-9.]+(\/.*)/);
+        return match && STANDARD_ROUTES.has(match[1]);
     }
 }
 
